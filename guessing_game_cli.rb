2222e95eab8a1b006d7ed8895cfa2c8cc95b9bb8
Patrick Alexander <patrick_alexander@mail.com>
2019-09-10 17:59:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,6 @@ def run_guessing_game
   elsif input == "exit"
     puts "Goodbye!"
   else 
-    puts "Sorry! The computer guessed #{number}."
+    puts "Sorry! The computer guessed #{number}. Try again! :)"
   end
 end 
