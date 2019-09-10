62fba03ea6efae95aa5d458870d7add411d6196e
Patrick Alexander <patrick_alexander@mail.com>
2019-09-10 17:56:19 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -10,13 +10,3 @@ def run_guessing_game
     puts "Sorry! The computer guessed #{number}."
   end
 end 
-#rando = 1+rand(6)
-  #puts "Guess a number between 1 and 6:"
-  #input = gets.chomp
-  #if input == rando.to_s
-    #puts "You guessed the correct number!"
-  #elsif input.downcase == "exit"
-    #puts "Goodbye!"
-  #else
-    #puts "Sorry! The computer guessed #{rando}."
-  #end
